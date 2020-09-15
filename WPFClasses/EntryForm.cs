b820dec7d1eb9c4bd775204951d08681b9cae68c
kendyllgavras <59926096+kendyllgavras@users.noreply.github.com>
2020-09-15 09:39:49 -0500

    update

---
+++ WPFClasses/EntryForm.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPFClasses
+{
+    class EntryForm
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public int ZipCode { get; set; }
+
+
+        public EntryForm()
+        {
+            Name = "";
+            Address = "";
+            ZipCode = 0;
+        }
+
+        public EntryForm(string name, string address, int zipcode)
+        {
+            Name = name;
+            Address = address;
+            ZipCode = zipcode;
+        }
+
+
+        public override string ToString()
+        {
+            string message = "Name:" + Name + "Address:" + Address + "ZipCode:" + ZipCode + ".";
+            return base.ToString(message);
+        }
+
+
+
+
+
+    }
+}
