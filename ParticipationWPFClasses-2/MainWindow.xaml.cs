a825a9c614083285aa7684468dfc955f1b4cfabf
kendyllgavras <59926096+kendyllgavras@users.noreply.github.com>
2020-09-21 12:22:20 -0500

    done

---
+++ ParticipationWPFClasses-2/MainWindow.xaml.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ParticipationWPFClasses_2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void TextBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+            Toy Toy1 = new Toy();
+            Toy1.Name = txtbxtoy.Text;
+            Toy1.Manufacturer = txtbxmanufacturer.Text;
+            Toy1.Price = Convert.ToDouble(txtbxprice.Text);
+            
+            
+            
+            listbox.Items.Add(Toy1);
+
+            txtbxmanufacturer.Clear();
+            txtbxprice.Clear();
+            txtbxtoy.Clear();
+        }
+
+        private void listbox_MouseDoubleClick(object sender, MouseButtonEventArgs e)
+        {
+            Toy selectedtoy = (Toy)listbox.SelectedItem;
+            MessageBox.Show(selectedtoy.GetAisle());
+        }
+
+ 
+    }
+}
---
+++ ParticipationWPFClasses-2/Toy.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ParticipationWPFClasses_2
+{
+    class Toy
+    {
+
+
+        public string Manufacturer { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
+
+        private string Aisle;
+
+
+
+        public Toy()
+        {
+            Manufacturer = "";
+            Name = "";
+            Price = 0;
+            Aisle = "";
+
+        }
+
+      
+        public string GetAisle()
+        {
+
+            string codeforgetaisle = ($"{Manufacturer[0]}{Price}");
+            return codeforgetaisle;
+        }
+
+        public override string ToString()
+        {
+            string showinbox = ($"{Manufacturer}-{Name}");
+            return showinbox;
+        }
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
