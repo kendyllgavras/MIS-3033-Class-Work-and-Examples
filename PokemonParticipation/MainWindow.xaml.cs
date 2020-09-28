a8a1b58e529a5baf4a1fac3bf0826766cea853dd
kendyllgavras <59926096+kendyllgavras@users.noreply.github.com>
2020-09-28 12:08:30 -0500

    update

---
+++ PokemonParticipation/MainWindow.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PokemonParticipation
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            PokemonAPI pokemonAPIResults;
+            string apiURL = @"https://pokeapi.co/api/v2/pokemon?limit=0&offset=1100";
+
+            using (var client=new HttpClient())
+            {
+                string jsonresults = client.GetStringAsync(apiURL).Result;
+                pokemonAPIResults = JsonConvert.DeserializeObject<PokemonAPI>(jsonresults);
+            }
+
+            foreach (var character in pokemonAPIResults.results)
+            {
+                cbocharacters.Items.Add(character);
+            }
+        }
+    }
+}
---
+++ PokemonParticipation/PokemonAPI.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PokemonParticipation
+{
+    public class PokemonAPI
+    {
+        public List<AllResults> results { get; set; }
+
+
+    }
+
+    public class AllResults
+    {
+        public string name { get; set; }
+        public string url { get; set; }
+
+        public override string ToString()
+        {
+            return name;
+        }
+    }
+
+    public class PokemonSprite
+    {
+        public int Height { get; set; }
+        public int Weight { get; set; }
+    }
+
+    public class sprite
+    {
+        //info on one pokemon
+    }
+}
